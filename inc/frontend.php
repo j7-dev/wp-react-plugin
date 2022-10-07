07b6108e4b368f679d1f44e235887d577483c2c7
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-10-07 11:23:39 +0700

    Enqueue frontend script

--- inc/frontend.php
+++ inc/frontend.php
@@ -4,12 +4,15 @@ declare( strict_types = 1 );
 
 namespace Kucrut\ViteForWPExample\React\Frontend;
 
+use Kucrut\Vite;
+
 /**
  * Frontend bootstrapper
  *
  * @return void
  */
 function bootstrap(): void {
+	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_script' );
 	add_action( 'wp_footer', __NAMESPACE__ . '\\render_app' );
 }
 
@@ -17,5 +20,19 @@ function bootstrap(): void {
  * Render application's markup
  */
 function render_app(): void {
-	printf( '<div id="my-app"></div>' );
+	printf( '<div id="my-app" class="my-app"></div>' );
+}
+
+/**
+ * Enqueue script
+ */
+function enqueue_script(): void {
+	Vite\enqueue_asset(
+		dirname( __DIR__ ) . '/dist',
+		'src/main.jsx',
+		[
+			'handle' => 'vite-for-wp-react',
+			'in-footer' => true,
+		]
+	);
 }
