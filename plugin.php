d606307ad1960447f89839f8767a0f1d8b56089f
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-10-07 09:31:53 +0700

    Add plugin file

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Vite for WP example: React
+ * Description: A plugin to demonstrate Vite for WP integration.
+ * Author: Dzikri Aziz
+ * Author URI: https://dz.aziz.im
+ * License: GPLv2
+ * Version: 0.0.1
+ */
+
+namespace Kucrut\ViteForWPExample\React;
+
+require_once __DIR__ . '/vendor/autoload.php';
