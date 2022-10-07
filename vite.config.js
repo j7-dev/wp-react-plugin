35700f4cdb5e4b1201f790f0185f0c94004c294f
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-10-07 11:22:56 +0700

    Vite config: Use create_config() helper

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import create_config from '@kucrut/vite-for-wp';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default create_config('src/main.jsx', 'dist', {
+	plugins: [react()],
+});
