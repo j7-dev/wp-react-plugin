040f02781611a5a929f06bd7623b185ee7d2c16d
J7 <j7.dev.gg@outlook.com>
2023-03-18 10:39:37 +0800

    feat: add wplogo in home page

--- js/src/pages/index.tsx
+++ js/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from '../assets/images/react.svg'
 import viteLogo from '../assets/images/vite.svg'
+import wpLogo from '../assets/images/wp.png'
 
 import { Link } from 'react-router-dom'
 
@@ -12,16 +13,23 @@ function DefaultPage() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <div className="flex justify-center">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer noopener">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer noopener">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a
+          href="https://wordpress.org"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={wpLogo} className="logo wp" alt="WordPress logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="flex justify-center">
+      <div className="flex justify-center mb-8">
         <button onClick={() => setCount((theCount) => theCount + 1)}>
           Count is {count}
         </button>
@@ -36,7 +44,7 @@ function DefaultPage() {
         Edit <code>src/App.tsx</code> and save to test HMR
       </p>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite, React and WordPress logos to learn more
       </p>
     </div>
   )
