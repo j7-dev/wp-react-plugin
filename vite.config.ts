65bdc65baffd5fd5f1544bd31e991ae214286347
J7 <j7.dev.gg@outlook.com>
2024-01-27 17:19:30 +0800

    chore: update Tailwindcss to 3.4.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
-import create_config from '@kucrut/vite-for-wp'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import alias from '@rollup/plugin-alias'
 import path from 'path'
 import liveReload from 'vite-plugin-live-reload'
+import { v4wp } from '@kucrut/vite-for-wp'
 
-export default create_config('js/src/main.tsx', 'js/dist', {
+export default {
   plugins: [
     alias(),
     react(),
     tsconfigPaths(),
     liveReload(__dirname + '/**/*.php'),
+    v4wp({
+      input: 'js/src/main.tsx', // Optional, defaults to 'src/main.js'.
+      outDir: 'js/dist', // Optional, defaults to 'dist'.
+    }),
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'js/src'),
     },
   },
-})
+}
