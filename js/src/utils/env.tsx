02f6695521c1a973d3f66ed69893f20b2a394987
J7 <j7.dev.gg@outlook.com>
2024-01-27 21:36:15 +0800

    refactor: env

--- js/src/utils/env.tsx
+++ js/src/utils/env.tsx
@@ -1,15 +1,12 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
-
-export const snake = 'my_app'
-const APP_DOMAIN = `${snake}_data` as string
+const APP_DOMAIN = `my_app_data` as string
+export const snake = window?.[APP_DOMAIN]?.env?.SNAKE || 'my_app'
 export const appName = window?.[APP_DOMAIN]?.env?.APP_NAME || 'My App'
-export const kebab = appName.toLowerCase().replace(/\s+/g, '-')
-
+export const kebab = window?.[APP_DOMAIN]?.env?.KEBAB || 'my-app'
 export const renderId1 = window?.[APP_DOMAIN]?.env?.RENDER_ID_1 || 'my_app'
 export const renderId2 =
   window?.[APP_DOMAIN]?.env?.RENDER_ID_2 || 'my_app_metabox'
-
 export const apiUrl = window?.wpApiSettings?.root || '/wp-json'
 export const ajaxUrl =
   window?.[APP_DOMAIN]?.env?.ajaxUrl || '/wp-admin/admin-ajax.php'
@@ -17,5 +14,4 @@ export const siteUrl = window?.[APP_DOMAIN]?.env?.siteUrl || '/'
 export const currentUserId = window?.[APP_DOMAIN]?.env?.userId || '0'
 export const postId = window?.[APP_DOMAIN]?.env?.postId || '0'
 export const permalink = window?.[APP_DOMAIN]?.env?.permalink || '/'
-
 export const apiTimeout = import.meta.env.API_TIMEOUT || '30000'
