f70d7a52d694e0f4216b5563963e753b9a14aed2
J7 <j7.dev.gg@outlook.com>
2024-02-18 20:02:20 +0800

    change ver

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name:       WP React Plugin (DEV)
  * Plugin URI:        https://github.com/j7-dev/wp-react-plugin
  * Description:       WP React Plugin is a boilerplate for creating a WordPress plugin with React, Tailwind, TypeScript, React Query v4, SCSS and Vite.
- * Version:           1.2.0
+ * Version:           1.3.0
  * Requires at least: 5.7
  * Requires PHP:      7.4
  * Author:            J7
