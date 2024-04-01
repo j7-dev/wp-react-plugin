7e24f46c59229eb0cf988b22099fb99e06540c01
J7 <j7.dev.gg@outlook.com>
2024-04-02 00:27:43 +0800

    feat: chain the command

--- release/.release-it.cjs
+++ release/.release-it.cjs
@@ -21,11 +21,19 @@ module.exports = {
     push: true,
   },
   hooks: {
-    'before:init': ['yarn build'], // run before initialization
+    'before:init': [
+      'yarn build',
+      'yarn create:release',
+      'composer install --no-dev',
+      'yarn zip',
+    ], // run before initialization
     // 'after:[my-plugin]:bump': './bin/my-script.sh', // run after bumping version of my-plugin
     // 'after:bump': ['echo finish'], // run after bumping version
     // 'after:git:release': 'echo After git push, before github release', // run after git push, before github release
-    // 'after:release': 'echo Successfully released ${name} v${version} to ${repo.repository}.', // run after release
+    'after:release': [
+      'git pull',
+      'echo Successfully released ${name} v${version} to ${repo.repository}.',
+    ], // run after release
   },
   npm: {
     publish: false,
