6266c38d7ab1f68b5793365b86613599306df383
J7 <j7.dev.gg@outlook.com>
2024-04-01 21:10:02 +0800

    feat: rename from json to js

---
+++ .release-it.cjs
@@ -0,0 +1,27 @@
+const path = require('path')
+
+const asset = path.resolve(__dirname, './release/wp-react-plugin.zip')
+
+module.exports = {
+  git: {
+    commit: true,
+    commitMessage: 'chore: release v${version}',
+    tag: true,
+    tagName: 'v${version}',
+    commitArgs: ['-n'],
+    push: true,
+  },
+  hooks: {
+    'before:init': ['yarn build'],
+    'after:bump': ['echo ✅ finish'],
+  },
+  npm: {
+    publish: false,
+  },
+  github: {
+    release: true,
+    releaseName: 'test v${version}',
+    assets: [asset],
+    web: false,
+  },
+}
