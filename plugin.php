594e1e88e8a5c7614d95f6d56a984c8af8a3fa4f
J7 <j7.dev.gg@outlook.com>
2024-04-03 00:29:51 +0800

    chore: release v2.0.9

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WP React Plugin (DEV)
  * Plugin URI:        https://github.com/j7-dev/wp-react-plugin
  * Description:       WP React Plugin is a boilerplate for creating a WordPress plugin with React, Tailwind, TypeScript, React Query v4, SCSS and Vite.
- * Version:           2.0.8
+ * Version:           2.0.9
  * Requires at least: 5.7
  * Requires PHP:      7.4
  * Author:            J7
