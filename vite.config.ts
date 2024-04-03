83c535bf7e4b3a272c12c3752ffbc4360a017201
TW-IT-Jerry Liu <jerry.liu@shoalter.com>
2024-04-03 13:19:43 +0800

    chore: mark livereload as optional for php

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import alias from '@rollup/plugin-alias'
 import path from 'path'
-import liveReload from 'vite-plugin-live-reload'
+
+// import liveReload from 'vite-plugin-live-reload'
+
 import { v4wp } from '@kucrut/vite-for-wp'
 
 export default {
@@ -10,7 +12,9 @@ export default {
     alias(),
     react(),
     tsconfigPaths(),
-    liveReload(__dirname + '/**/*.php'),
+
+    // liveReload(__dirname + '/**/*.php'), // Optional, if you want to reload page on php changed
+
     v4wp({
       input: 'js/src/main.tsx', // Optional, defaults to 'src/main.js'.
       outDir: 'js/dist', // Optional, defaults to 'dist'.
