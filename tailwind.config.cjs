2144723c2ade24c630ff92fb239dc48abf644706
J7 <j7.dev.gg@outlook.com>
2024-04-14 23:36:52 +0800

    feat: modify tailwind content and phpcs to test php 8

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  important: true,
   corePlugins: {
     preflight: false,
   },
-  content: ['./js/src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./js/src/**/*.{js,ts,jsx,tsx}', './inc/**/*.php'],
   theme: {
     extend: {
       colors: {
@@ -14,7 +13,7 @@ module.exports = {
       screens: {
         sm: '576px', // iphone SE
         md: '810px', // ipad Portrait
-        lg: '1080px', // ipad Landscape 
+        lg: '1080px', // ipad Landscape
         xl: '1280px', // mac air
         xxl: '1440px',
       },
