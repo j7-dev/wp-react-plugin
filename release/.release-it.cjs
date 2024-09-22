d2aafa75ed3ffde97dd59c187eade9912aba2635
J7 <j7.dev.gg@outlook.com>
2024-09-22 19:02:58 +0800

    feat: move i18n command to before:init

--- release/.release-it.cjs
+++ release/.release-it.cjs
@@ -28,10 +28,9 @@ module.exports = {
 		push: release,
 	},
 	hooks: {
-		// 'before:init': [], // run before initialization
+		'before:init': ['yarn i18n'], // run before initialization
 		// 'after:[my-plugin]:bump': './bin/my-script.sh', // run after bumping version of my-plugin
 		'after:bump': [
-			'yarn i18n',
 			'yarn build && echo ✅ build success',
 			release
 				? 'yarn sync:version && echo ✅ sync version success'
