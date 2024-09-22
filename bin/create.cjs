13f40053fcbb8d50c61b14b8e53556c9c373a5e8
J7 <j7.dev.gg@outlook.com>
2024-09-22 18:58:44 +0800

    feat: remove languages after project create

--- bin/create.cjs
+++ bin/create.cjs
@@ -5,20 +5,22 @@ const { execRemove, execRemoveFile } = require('./utils/remove.cjs')
 const projectName = process?.argv?.[2] || ''
 
 async function main() {
-  // can be shell command or callback function
+	// can be shell command or callback function
 
-  const commands = [
-    `git clone https://github.com/j7-dev/wp-react-plugin.git ${projectName}`,
-    `cd ${projectName}`,
-    'yarn',
-    `node ./replace-string.cjs ${projectName}`,
-    'composer install --no-interaction',
-    execRemove('.git'),
-    execRemove('bin'),
-    execRemoveFile('replace-string.cjs'),
-    execRemoveFile('README.md'),
-  ]
-  const results = await runCommands(...commands)
+	const commands = [
+		`git clone https://github.com/j7-dev/wp-react-plugin.git ${projectName}`,
+		`cd ${projectName}`,
+		'yarn',
+		`node ./replace-string.cjs ${projectName}`,
+		'composer install --no-interaction',
+		execRemove('.git'),
+		execRemove('bin'),
+		execRemoveFile('replace-string.cjs'),
+		execRemoveFile('README.md'),
+		execRemove('languages'),
+		'mkdir languages',
+	]
+	const results = await runCommands(...commands)
 }
 
 main()
