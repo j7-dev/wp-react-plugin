94d6b0a28045c3ac7c3b44b40f8720a746f91b7a
JerryLiu <j7.dev.gg@gmail.com>
2025-01-22 17:41:00 +0800

    feat: lower css priority of antd

--- js/src/main.tsx
+++ js/src/main.tsx
@@ -37,7 +37,7 @@ mapping.forEach(({ els, App }) => {
 			ReactDOM.createRoot(el).render(
 				<React.StrictMode>
 					<QueryClientProvider client={queryClient}>
-						<StyleProvider hashPriority="high">
+						<StyleProvider hashPriority="low">
 							<App />
 						</StyleProvider>
 						<ReactQueryDevtools initialIsOpen={false} />
